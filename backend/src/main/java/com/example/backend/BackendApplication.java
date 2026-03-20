179ee590bb5ff297bf1f1a7c9b229ef24ba0f116
dangphuc2611 <cuccukgiontan2611@gmail.com>
2026-03-20 16:39:53 +0700

    refactor: Update database connection system property names from generic to specific.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -12,9 +12,11 @@ public class BackendApplication {
 		Dotenv dotenv = Dotenv.configure()
 				.ignoreIfMissing()
 				.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("HOST", dotenv.get("HOST"));
+		System.setProperty("PORT", dotenv.get("PORT"));
+		System.setProperty("DATABASE", dotenv.get("DATABASE"));
+		System.setProperty("USER", dotenv.get("USER"));
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
 		System.setProperty("JPA_DDL_AUTO", dotenv.get("JPA_DDL_AUTO", "update"));
 		System.setProperty("JPA_SHOW_SQL", dotenv.get("JPA_SHOW_SQL", "true"));
 		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT", "8080"));
