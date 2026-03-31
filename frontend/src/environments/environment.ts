b312bd9d831777133654f583431a7ff509628076
dangphuc2611 <cuccukgiontan2611@gmail.com>
2026-04-01 02:43:21 +0700

    fix: downgrade angular and add npmrc for vercel deploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-    apiUrl: 'https://kickoff-project-production.up.railway.app',
+    apiUrl: 'https://individual-kickoff-project-production.up.railway.app',
 };
